274b8d1cc341d8d4b7c717f7e3c258d2395c39cd
Omar <omar.2005.feb.12@gmail.com>
2025-06-15 02:13:33 +0530

    filled_sorries

--- DehnFunction/Basic.lean
+++ DehnFunction/Basic.lean
@@ -2,7 +2,8 @@ import Mathlib.GroupTheory.PresentedGroup
 import Mathlib.Data.Nat.Find
 import Mathlib.Data.Nat.Lattice
 import Mathlib.GroupTheory.FreeGroup.Reduce
-
+import Mathlib.Algebra.Group.Subgroup.Lattice
+import Mathlib.Algebra.Group.Subgroup.Finite
 
 #check Group.conjugatesOfSet
 
@@ -14,9 +15,14 @@ n - natural number
 This predicate states that the word `w` is a product of `n` conjugates of elements from the set `R`.
 
 -/
-def IsProductOfNConjugates {G: Type*}(R : Set (FreeGroup G)) (n : ℕ) (w : FreeGroup G) : Prop :=
-  ∃ (l : List {c // c ∈ Group.conjugatesOfSet R}),
+def IsProductOfNConjugates₂ {G: Type*}(R : Set (FreeGroup G)) (n : ℕ) (w : FreeGroup G) : Prop :=
+  ∃ (l : List {c // c ∈ Group.conjugatesOfSet R ∨ c⁻¹ ∈ Group.conjugatesOfSet R}),
     l.length = n ∧ w = (l.map Subtype.val).prod
+
+def IsProductOfNConjugates {G: Type*}(R : Set (FreeGroup G)) (n : ℕ) (w : FreeGroup G) : Prop :=
+  ∃ (l : List (FreeGroup G)),(∀ c ∈ l, c ∈ Group.conjugatesOfSet R ∨ c⁻¹ ∈ Group.conjugatesOfSet R) ∧
+    l.length = n ∧ w = l.prod
+
 
 inductive MyGen | a | b
    deriving DecidableEq, Repr
@@ -44,10 +50,17 @@ lemma dumb : IsProductOfNConjugates R 1 w := by
     · exact Set.mem_singleton w
     . use 1
       simp
-  use [{val := a' * b' * a'⁻¹ * b'⁻¹, property :=by exact h_w_conj}]
+  use [w]
   constructor
-  · rfl
-  · rfl
+  . intro c h_c_in
+    rw [List.mem_singleton] at h_c_in
+    left
+    unfold R
+    exact Set.mem_of_eq_of_mem h_c_in h_w_conj
+  . constructor
+    . simp
+    . simp
+
 
 end MyGen
 
@@ -79,12 +92,11 @@ example : wordArea R w = 1 := by
       aesop
 
     have h_w_is_1 : w=1 :=by
-      rcases h_0_in with ⟨l, h_len, h_prod⟩
-      have h_l_empty : l = [] := by
-        exact List.eq_nil_iff_length_eq_zero.mpr h_len
-      rw [h_l_empty] at h_prod
-      simp at h_prod
-      exact h_prod
+      have h_prod_of_0 : IsProductOfNConjugates R 0 w := by
+        aesop
+      unfold IsProductOfNConjugates at h_prod_of_0
+      aesop
+
     have h_w_ne_1 : w ≠ 1 := by
       rw [w]
       intro h_eq_1
@@ -117,13 +129,11 @@ example: wordArea₂ R w := {
       exact Nat.le_of_lt_succ h_gt0
     rw [h_m_0] at h_lt
     have h_w_is_1 : w = 1 := by
-      unfold IsProductOfNConjugates at h_lt
-      rcases h_lt with ⟨l, h_len, h_prod⟩
-      have h_l_empty : l = [] := by
-        exact List.eq_nil_iff_length_eq_zero.mpr h_len
-      rw [h_l_empty] at h_prod
-      simp at h_prod
-      exact h_prod
+      have h_prod_of_0 : IsProductOfNConjugates R 0 w := by
+        aesop
+      unfold IsProductOfNConjugates at h_prod_of_0
+      aesop
+
     have h_w_ne_1 : w ≠ 1 := by
       rw [w]
       intro h_eq_1
@@ -140,8 +150,250 @@ example: wordArea₂ R w := {
 
 end MyGen
 
+theorem wordArea_one {G : Type*} (R : Set (FreeGroup G)) : wordArea R 1 = 0 := by
+  unfold wordArea
+  refine Nat.sInf_eq_zero.mpr ?_
+  left
+  unfold IsProductOfNConjugates
+  use []
+  simp
+
+--# Important Theorems for normal closure
+
+theorem list_prod_inv {G: Type*}[Group G](l : List G) :
+    (l.map Inv.inv).reverse.prod = (l.prod)⁻¹ := by
+  induction l with
+  | nil =>
+    simp
+  | cons hd tl ih =>
+
+    simp [List.prod_cons, mul_inv_rev, List.map_cons, List.reverse_cons,
+               List.prod_append, List.prod_singleton]
+
+    rw [ih]
+
+
+theorem exists_list_of_mem_closure' {G : Type*} [Group G] {s : Set G} {x : G} :
+    x ∈ Subgroup.closure s ↔ ∃ l : List G, (∀ y ∈ l, y ∈ s ∨ y⁻¹ ∈ s) ∧ l.prod = x := by
+
+  constructor
+
+
+  . intro hx
+
+    induction hx using Subgroup.closure_induction with
+
+
+    | mem g hg =>
+
+      use [g]
+      constructor
+      .
+        intro y hy_mem_list
+        rw [List.mem_singleton.mp hy_mem_list]
+        left
+        exact hg
+      .
+        simp
+
+    | one =>
+
+      use []
+
+      simp
+
+    | mul x y _ _ ih_x ih_y =>
+
+      rcases ih_x with ⟨l₁, h_l₁_prop, h_l₁_prod⟩
+      rcases ih_y with ⟨l₂, h_l₂_prop, h_l₂_prod⟩
+
+      use l₁ ++ l₂
+      constructor
+      .
+        intro z hz_mem_list
+        rw [List.mem_append] at hz_mem_list
+
+        cases hz_mem_list with
+        | inl h => exact h_l₁_prop z h
+        | inr h => exact h_l₂_prop z h
+      .
+
+        simp [h_l₁_prod, h_l₂_prod]
+
+    | inv x _ ih_x =>
+
+      rcases ih_x with ⟨l, h_l_prop, h_l_prod⟩
+
+      use (l.map Inv.inv).reverse
+      constructor
+      .
+        intro y hy_mem_list
+
+        rw [List.mem_reverse, List.mem_map] at hy_mem_list
+        rcases hy_mem_list with ⟨z, hz_mem_l, rfl⟩
+        simp
+        exact Or.symm (h_l_prop z hz_mem_l)
+
+      . rw [← h_l_prod]
+        rw [list_prod_inv]
+
+
+  . rintro ⟨l, h_l_prop, h_l_prod⟩
+
+    have h_all_mem_closure : ∀ y ∈ l, y ∈ Subgroup.closure s := by
+      intros y hy_mem_l
+
+      cases h_l_prop y hy_mem_l with
+      | inl h_y_in_s =>
+
+        exact Subgroup.subset_closure h_y_in_s
+      | inr h_y_inv_in_s =>
+        have h_yinv : y⁻¹ ∈ Subgroup.closure s := by
+          exact Subgroup.subset_closure h_y_inv_in_s
+        exact (Subgroup.inv_mem_iff (Subgroup.closure s)).mp h_yinv
+
+
+    rw [← h_l_prod]
+    exact Subgroup.list_prod_mem (Subgroup.closure s) h_all_mem_closure
+
+
+
+theorem mem_normalClosure_iff_prod_conj {G : Type*} [Group G](R : Set G) (w: G): w∈ Subgroup.normalClosure R ↔ ∃ (l: List G), (∀ x ∈ l, x∈ Group.conjugatesOfSet R ∨ x⁻¹ ∈ Group.conjugatesOfSet R) ∧ w = l.prod := by
+  rw[Subgroup.normalClosure]
+  rw[ exists_list_of_mem_closure']
+  apply Iff.intro
+  · intro a
+    obtain ⟨w_1, h⟩ := a
+    obtain ⟨left, right⟩ := h
+    subst right
+    apply Exists.intro
+    · apply And.intro
+      intro x a
+      on_goal 2 => {rfl
+      }
+      simp_all only
+  · intro a
+    obtain ⟨w_1, h⟩ := a
+    obtain ⟨left, right⟩ := h
+    subst right
+    apply Exists.intro
+    · apply And.intro
+      intro y a
+      on_goal 2 => {rfl
+      }
+      simp_all only
+
+theorem wordArea_eq_zero_iff {G : Type*} (R : Set (FreeGroup G)) (w : FreeGroup G) :
+  wordArea R w = 0 ↔ w = 1 ∨ w ∉ Subgroup.normalClosure R := by
+  constructor
+  . intro h_area_0
+    unfold wordArea at h_area_0
+    apply Nat.sInf_eq_zero.mp at h_area_0
+    rcases h_area_0 with ⟨n, h_prod⟩
+    case mp.inr k =>
+      right
+      unfold IsProductOfNConjugates at k
+      intro h_in_closure
+
+      rw [mem_normalClosure_iff_prod_conj] at h_in_closure
+
+      rcases h_in_closure with ⟨l, h_l_prop, h_w_prod⟩
+
+      have h_is_in_set : l.length ∈ {n | ∃ l': List (FreeGroup G), (∀ c ∈ l', c ∈ Group.conjugatesOfSet R ∨ c⁻¹ ∈ Group.conjugatesOfSet R) ∧ l'.length = n ∧ w = l'.prod} := by
+        use l
+
+      have h_not_in_set := Set.eq_empty_iff_forall_notMem.mp k
+      specialize h_not_in_set l.length
+
+      exact h_not_in_set h_is_in_set
+
+    case mp.inl =>
+      left
+      rcases h_prod with ⟨l, h_prod⟩
+      have h_l_empty : n = [] := by
+        aesop
+      rw [h_l_empty] at h_prod
+      simp at h_prod
+      exact h_prod
+  . intro h
+    cases h with
+    | inl h_eq_1 =>
+      unfold wordArea
+      rw [h_eq_1]
+      exact wordArea_one R
+    | inr h_not_in =>
+      unfold wordArea
+      refine Nat.sInf_eq_zero.mpr ?_
+      right
+      unfold IsProductOfNConjugates
+      apply Set.eq_empty_iff_forall_notMem.mpr
+
+      intro n h_n_in_set
+
+      rcases h_n_in_set with ⟨l, h_l_prop, h_n_len, h_w_prod⟩
+
+      have h_in_closure : w ∈ Subgroup.normalClosure R := by
+
+        refine (mem_normalClosure_iff_prod_conj R w).mpr ?_
+
+        use l
+
+      exact h_not_in h_in_closure
+
+theorem conjugatesOfSet_of_empty {G: Type*} [Group G] :
+  Group.conjugatesOfSet (∅: Set G) = ∅ := by
+  unfold Group.conjugatesOfSet
+  simp
+
+
+theorem wordArea_free_grp_is_zero {G: Type*} [DecidableEq G] (w : FreeGroup G) :
+  wordArea ∅ w = 0 := by
+  rw [wordArea_eq_zero_iff]
+
+  by_cases h_w_1 : w = 1
+  . left
+    exact h_w_1
+
+  . right
+    intro h_in_closure
+    unfold Subgroup.normalClosure at h_in_closure
+    rw [conjugatesOfSet_of_empty] at h_in_closure
+    simp at h_in_closure
+    contradiction
+
+
+
 
 --# Dehn Functions
 
-noncomputable def dehnFunction {G: Type*} [DecidableEq G] (R : Set (FreeGroup G)) (n: ℕ ): ℕ :=
+noncomputable def dehn {G: Type*} [DecidableEq G] (R : Set (FreeGroup G)) (n: ℕ ): ℕ :=
   sSup (wordArea R '' {w | FreeGroup.norm w ≤ n})
+
+
+theorem dehn_free_grp_is_zero {G: Type*} [DecidableEq G](n: ℕ) :
+  dehn (∅: Set (FreeGroup G)) n = 0 := by
+  unfold dehn
+  by_cases h_non_empty: {w: FreeGroup G | FreeGroup.norm w ≤ n} = ∅
+  . rw [h_non_empty]
+    simp
+  .
+    have h_empty : wordArea ∅ '' {w: FreeGroup G | FreeGroup.norm w ≤ n} = {0} := by
+      simp [wordArea_free_grp_is_zero]
+      ext x
+      simp
+      constructor
+      . intro h_x_in
+        simp[h_x_in]
+      . intro h_x_eq_0
+        simp[h_x_eq_0]
+        have h_n_empty: {w: FreeGroup G | FreeGroup.norm w ≤ n}.Nonempty := by
+          exact Set.nonempty_iff_ne_empty.mpr h_non_empty
+        rw [Set.nonempty_def] at h_n_empty
+        subst h_x_eq_0
+        simp_all only [Set.mem_setOf_eq]
+
+
+
+
+    rw [h_empty]
+    simp
