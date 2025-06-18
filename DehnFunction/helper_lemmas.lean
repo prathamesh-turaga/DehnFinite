0e326e1ccaa24c4b280a77f91355076eab26a344
ArjSheth <asheth11819513@gmail.com>
2025-06-18 16:13:24 +0530

    Create helper_lemmas.lean

---
+++ DehnFunction/helper_lemmas.lean
@@ -0,0 +1,14 @@
+-- iff : (step R x y), (xy⁻¹ or yx⁻¹ ∈ ConjSet (R))
+import Mathlib.GroupTheory.PresentedGroup
+import Mathlib.Data.Nat.Find
+import Mathlib.Data.Nat.Lattice
+import Mathlib.GroupTheory.FreeGroup.Reduce
+import Mathlib.Algebra.Group.Subgroup.Lattice
+import Mathlib.Algebra.Group.Subgroup.Finite
+import Mathlib.Data.Real.Basic
+import Mathlib.Algebra.Group.Subgroup.Ker
+import Mathlib.Algebra.BigOperators.Group.List.Basic
+import Mathlib.Data.List.Rotate
+import DehnFunction.Area1
+import DehnFunction.Area2
+import DehnFunction.Area_equiv
