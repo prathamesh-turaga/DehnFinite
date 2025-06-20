bd9d1ba1f1fa9d681452e302c93b0bbd8de70bb0
Omar <omar.2005.feb.12@gmail.com>
2025-06-20 11:07:32 +0530

    helper_lemmas

--- DehnFunction/Area2.lean
+++ DehnFunction/Area2.lean
@@ -5,10 +5,10 @@ import Mathlib.Data.List.Rotate
 import Mathlib.Data.Nat.Find
 import Mathlib.Data.Nat.Lattice
 import DehnFunction.Area1
+import DehnFunction.helper_lemmas
 
-variable {α : Type*}
-variable [DecidableEq α]
-variable (trial_set : Set (FreeGroup α))
+
+
 
 def CycPermList {α : Type*} [DecidableEq α] : (relator : FreeGroup α) → List (List (α × Bool)):= fun relator => List.cyclicPermutations (FreeGroup.toWord relator)
 -- This function takes a freeword (if the underlying type for the freegroup is decidable) and gives us all its cyclic permutations as a list of lists.
@@ -21,42 +21,6 @@ def CycPerm {α : Type*} [DecidableEq α] (w : FreeGroup α) := list_to_free (Cy
 
 
 
-def ReduceMyPairs {α : Type*} [DecidableEq α] (L : List (α × Bool)) : List (α × Bool) :=
-  match L with
-
-  | [] => []
-  | [_] => L
-
-  | x :: y :: ys =>
-
-      let xs := y :: ys
-
-      let last := xs.getLast (by simp)
-      let middle := xs.dropLast
-
-      if x.1 = last.1 ∧ x.2 ≠ last.2 then
-        ReduceMyPairs middle
-      else
-        L
-termination_by L.length
-
-
-#check ReduceMyPairs
-
-def CycRed {α : Type*} [DecidableEq α] (w : FreeGroup α) := FreeGroup.mk (ReduceMyPairs (FreeGroup.toWord w))
---Cyclically reduces a freeword
-
-def CyclicPermutationsOfRelators {G : Type*} [DecidableEq G] (R : Set (FreeGroup G)) : Set (FreeGroup G) :=
-
-  ⋃ r ∈ R,
-    { elem |
-      ∃ word ∈ (FreeGroup.toWord r).cyclicPermutations,
-      elem = FreeGroup.mk word
-    }
-
-
-def step {γ : Type*} [DecidableEq γ] (RelatorSet : Set (FreeGroup γ)) (w₁ w₂ : FreeGroup γ) : Prop :=
-  ((CycRed (w₁ * w₂⁻¹)) ∈ CyclicPermutationsOfRelators RelatorSet) ∨ ((CycRed (w₂ * w₁⁻¹)) ∈ CyclicPermutationsOfRelators RelatorSet)
 
 def step_n {α : Type*} [DecidableEq α] (relators : Set (FreeGroup α)) (n : ℕ) (w1 w2 : FreeGroup α) : Prop :=
   match n with
@@ -71,10 +35,7 @@ noncomputable def Area2 {α : Type*} [DecidableEq α] (relators : Set (FreeGroup
 
 
 
-theorem step_iff_conjugate {G : Type*} [DecidableEq G] {R : Set (FreeGroup G)} (x y : FreeGroup G):
-    (step R x y) ↔
-  x*y⁻¹ ∈ Group.conjugatesOfSet R ∨ y*x⁻¹ ∈ Group.conjugatesOfSet R := by
-    sorry
+
 
 theorem wordArea_eq_zero_iff2 {G : Type*} [DecidableEq G](R : Set (FreeGroup G)) (w : FreeGroup G) :
   Area2 R w = 0 ↔ w = 1 ∨ w ∉ Subgroup.normalClosure R := by
@@ -224,164 +185,3 @@ theorem step_empty {G : Type*} [DecidableEq G] (R : Set (FreeGroup G)) :
 
 
   use l
-
-
-theorem reduceMyPairs_property {α : Type*} [DecidableEq α] (L : List (α × Bool)) :
-    ∃ (U V : List (α × Bool)), L = U ++ ReduceMyPairs L ++ V ∧ FreeGroup.mk (V ++ U) = 1 := by
-
-  match h_L_eq : L with
-
-  | [] =>
-
-    use [], []
-    simp
-    constructor
-    . unfold ReduceMyPairs
-      simp
-    .
-     subst h_L_eq
-     rfl
-
-
-  | [x] =>
-    use [], []
-    simp
-    constructor
-    . unfold ReduceMyPairs
-      simp
-    .
-     subst h_L_eq
-     rfl
-  | x :: y :: ys =>
-    let xs := y :: ys
-    have h_L_form : L = x :: xs := by
-      simp [h_L_eq]
-      simp[xs]
-
-    let last := xs.getLast (by simp)
-    let middle := xs.dropLast
-    if h_if : x.1 = last.1 ∧ x.2 ≠ last.2 then
-      have h_def : ReduceMyPairs L = ReduceMyPairs middle := by
-        rw [ReduceMyPairs.eq_def]
-        rw [h_L_form]
-        aesop
-
-      have ih := reduceMyPairs_property middle
-      rcases ih with ⟨U', V', h_middle_decomp, h_vu'_is_one⟩
-
-      let U := [x] ++ U'
-      let V := V' ++ [last]
-      use U, V
-
-      constructor
-
-
-      · rw[← h_L_eq]
-        dsimp [U, V]
-        rw [h_def, h_middle_decomp]
-
-        rw [h_L_form]
-        simp only [List.append_assoc, List.cons_append, ← h_middle_decomp]
-
-        rw [show xs = middle ++ [last]
-        by
-          change xs = xs.dropLast ++ [xs.getLast _]
-          exact
-            Eq.symm
-              (List.dropLast_concat_getLast
-                (of_eq_true
-                  (Eq.trans (congrArg Not (eq_false' fun h ↦ List.noConfusion h))
-                    not_false_eq_true)))
-          ]
-        simp
-        rw[← List.append_assoc]
-        rw [← List.append_assoc ]
-        rw[← h_middle_decomp]
-
-
-      · dsimp [U, V]
-        rw [List.append_assoc]
-
-        rw [← FreeGroup.mul_mk, ← FreeGroup.mul_mk]
-        change FreeGroup.mk V' * (FreeGroup.mk [last] * FreeGroup.mk ([x] ++ U')) = 1
-        rw [← FreeGroup.mul_mk]
-
-        have h_cancel : FreeGroup.mk [last] * FreeGroup.mk [x] = 1 := by
-          have h_fst : x.1 = last.1 := by
-            simp [h_if]
-          have h_snd : (x.2) ≠  last.2 := by
-            simp [h_if]
-          have h_snd' : last.2 = !x.2 := by
-            exact Bool.eq_not.mpr (id (Ne.symm h_snd))
-
-
-          rw [mul_eq_one_iff_eq_inv]
-          rw [FreeGroup.inv_mk]
-          simp[FreeGroup.invRev]
-          change FreeGroup.mk [(last.1, last.2)] = FreeGroup.mk [(x.1, !x.2)]
-          rw[← h_fst, h_snd']
-
-        rw[← mul_assoc (FreeGroup.mk [last]) (FreeGroup.mk [x]) (FreeGroup.mk U')]
-        rw [h_cancel]
-        simp
-        exact h_vu'_is_one
-    else
-      have h_def : ReduceMyPairs L = L := by
-        rw [h_L_eq]
-        simp [ReduceMyPairs, h_if]
-        aesop
-
-
-      use [], []
-      rw[← h_L_eq]
-      rw [h_def]
-      aesop
-  termination_by L.length
-
-
-theorem cycRed_is_a_cyclic_permutation {G : Type*} [DecidableEq G] (y : FreeGroup G) :
-  CycRed y ∈ CyclicPermutationsOfRelators {y} := by
-
-  let L := FreeGroup.toWord y
-  let M := ReduceMyPairs L
-  unfold CyclicPermutationsOfRelators
-  simp
-
-  unfold CycRed
-
-  have h_prop := reduceMyPairs_property L
-  rcases h_prop with ⟨U, V, h_decomp, h_vu_is_one⟩
-
-
-  let p := M ++ V ++ U
-  use p
-
-
-  constructor
-
-  · change p ~r L
-    use (M ++ V).length
-    dsimp [p]
-    rw[h_decomp]
-    have h_M_def: ReduceMyPairs L = M := by
-
-      rfl
-    rw [h_M_def]
-    rw [List.rotate_eq_drop_append_take]
-    . simp
-      have h_assoc: M ++ (V ++ U) = (M ++ V) ++ U := by
-        simp
-      rw [h_assoc]
-      rw [List.take_append_of_le_length (by simp)]
-      simp
-    . simp
-  · dsimp [p]
-    change FreeGroup.mk M = FreeGroup.mk (M ++ V ++ U)
-    apply Eq.symm
-    calc
-      FreeGroup.mk (M ++ V ++ U) = FreeGroup.mk M * FreeGroup.mk (V ++ U) := by
-        simp
-      _ = FreeGroup.mk M * 1 := by
-        rw[h_vu_is_one]
-      _ = FreeGroup.mk M := by
-        simp
