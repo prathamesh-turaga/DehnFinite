70ce4210941bff8d343c1e4a9ee5c9b895a7e8e9
Vivek Rajesh Joshi <vivekjoshi306@yahoo.com>
2025-06-24 15:40:20 +0530

    Update helper_lemmas.lean

--- DehnFunction/helper_lemmas.lean
+++ DehnFunction/helper_lemmas.lean
@@ -347,57 +347,237 @@ lemma Uncycleconj_is_reduced_cperm {α : Type*} [DecidableEq α] : ∀ (g y : Fr
 
 
 
-
-
-
-def uncyclic {α : Type*} [DecidableEq α] (L : List (α × Bool)) : Prop := CycReduce L = L
-
--- def uncyclic_elt {α : Type*} [DecidableEq α] (w : FreeGroup α) : Prop := CycRed w = w
-
-section
-
-variable {α : Type*} [DecidableEq α] (L : List (α × Bool)) (w : FreeGroup α)
-
-instance : Decidable (uncyclic L) :=
-  inferInstanceAs (Decidable (CycReduce L = L))
-
--- instance : Decidable (uncyclic_elt w) :=
---   inferInstanceAs (Decidable (CycRed w = w))
-
-end
+namespace FreeGroup
 
 #check FreeGroup.reduce
 
--- def isCycRed {α : Type*} [DecidableEq α] (w1 : FreeGroup α) (w2 : FreeGroup α) (hw1 : uncyclic_elt w1) (hw2 : uncyclic_elt w2) : Prop :=
---   w1.toWord ∈ (w2.toWord.cyclicPermutations.map FreeGroup.reduce)
+def word_to_CycPermList {α : Type*} [DecidableEq α] (L : List (α×Bool)) : List (List (α × Bool)) := (List.map FreeGroup.reduce L.cyclicPermutations)
 
-def isCycPerm' {α : Type*} [DecidableEq α] (L1 L2 : List (α×Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2) : Prop :=
-  (FreeGroup.reduce L1) ∈ ((FreeGroup.reduce L2).cyclicPermutations)
-
-def isCycPerm {α : Type*} [DecidableEq α] (L1 L2 : List (α×Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2) : Prop :=
-  (FreeGroup.reduce L1).IsRotated (FreeGroup.reduce L2)
+def IsCycPerm {α : Type*} [DecidableEq α] (L1 L2 : List (α×Bool)) : Prop :=
+  (FreeGroup.reduce L1) ∈ (word_to_CycPermList L2)
 
 section
-
-variable {α : Type*} [DecidableEq α] (L1 L2 : List (α × Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2)
--- #synth Decidable (isCycPerm L1 L2 hL1 hL2)
-instance : Decidable (isCycPerm L1 L2 hL1 hL2) :=
-  inferInstanceAs (Decidable ((FreeGroup.reduce L1).IsRotated (FreeGroup.reduce L2)))
-
+variable {α : Type*} [DecidableEq α] (L1 L2 : List (α × Bool))
+instance : Decidable (IsCycPerm L1 L2) :=
+  inferInstanceAs (Decidable ((FreeGroup.reduce L1) ∈ (word_to_CycPermList L2)))
 end
 
-theorem isCycPerm_same {α : Type*} [DecidableEq α] (L1 L2 : List (α×Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2) : isCycPerm L1 L2 hL1 hL2 ↔ isCycPerm' L1 L2 hL1 hL2 := by
-  dsimp [isCycPerm,isCycPerm']
-  symm
-  exact List.mem_cyclicPermutations_iff
+-- def IsReduced {α : Type*} [DecidableEq α] (L : List (α × Bool)) : Prop := FreeGroup.reduce L = L
 
-theorem isCycPerm_refl {α : Type*} [DecidableEq α] (L1 : List (α × Bool)) (hL1 : uncyclic L1) : isCycPerm L1 L1 hL1 hL1 := by
-  dsimp [isCycPerm]
-  rfl
+@[simp]
+def IsReduced {α : Type*} [DecidableEq α] (l : List (α × Bool)) : Prop :=
+  ∀ l' : List (α×Bool), Red l l' → l' = l
 
-theorem isCycPerm_symm {α : Type*} [DecidableEq α] (L1 L2 : List (α × Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2) : isCycPerm L1 L2 hL1 hL2 → isCycPerm L2 L1 hL2 hL1 := by
-  dsimp [isCycPerm]
-  exact fun a ↦ id (List.IsRotated.symm a)
+theorem IsReduced_nil {α : Type*} [DecidableEq α] : IsReduced ([] : List (α×Bool)) := by
+  dsimp [IsReduced]
+  intro l
+  exact Red.nil_iff.mp
+
+def IsReduced' {α : Type*} [DecidableEq α] (L : List (α × Bool)) : Prop :=
+  match L with
+  | [] => True
+  | [_] => True
+  | a::b::as =>
+    IsReduced' (b::as) ∧ (a.1 ≠ b.1 ∨ a.2 = b.2)
+
+theorem IsReduced_same {α : Type*} [DecidableEq α] (L : List (α × Bool)) : IsReduced L ↔ IsReduced' L := by
+  constructor
+  · intro h
+    induction L using IsReduced'.induct with
+    | case1 => rw [IsReduced']; tauto
+    | case2 _ => rw [IsReduced']; tauto
+    | case3 a b bs ih =>
+      rw [IsReduced']
+      rw [IsReduced] at h
+      sorry
+  · sorry
+
+theorem IsReduced_singleton {α : Type*} [DecidableEq α] (a : α×Bool) : IsReduced [a] := by
+  dsimp
+  intro l
+  exact Red.singleton_iff.mp
+
+-- theorem IsReduced_two {α : Type*} [DecidableEq α] (a b : α×Bool) (h : IsReduced [a,b]) :
+--   a.1 ≠ b.1 ∨ a.2 = b.2 := by dsimp [IsReduced] at h; exact h.2
+
+-- theorem IsReduced_two_if {α : Type*} [DecidableEq α] (a b : α×Bool) (hab : a.1 ≠ b.1 ∨ a.2 = b.2) : IsReduced [a,b] := by simp [IsReduced,hab]
+
+theorem IsReduced_cons_cons {α : Type*} [DecidableEq α] (a b : α×Bool) (as : List (α × Bool)) (h : IsReduced (a::b::as)) :
+  IsReduced (b::as) ∧ (a.1 ≠ b.1 ∨ a.2 = b.2) := h
+
+theorem IsReduced_cons {α : Type*} [DecidableEq α] (a : α×Bool) (as : List (α × Bool)) (has : as ≠ []) (h : IsReduced (a::as)) :
+  IsReduced as ∧ (a.1 ≠ (as.head has).1 ∨ a.2 = (as.head has).2) := by
+  match as with
+  | b::bs =>
+    apply IsReduced_cons_cons at h
+    simp [h]
+
+-- theorem IsReduced_cons_if1 {α : Type*} [DecidableEq α] (a b : α×Bool) (tl : List (α × Bool)) (htl : IsReduced tl) (hab : a.1 ≠ b.1) : IsReduced (a::b::tl) := by
+--   rw [IsReduced]
+--   simp [htl,hab]
+
+-- theorem IsReduced_cons_if2 {α : Type*} [DecidableEq α] (a b : α×Bool) (tl : List (α × Bool)) (htl : IsReduced tl) (hab : a.2 = b.2) : IsReduced (a::b::tl) := by
+--   rw [IsReduced]
+--   simp [htl,hab]
+
+-- #eval (FreeGroup.reduce [])
+
+#print IsReduced
+
+theorem IsReduced_cons_if {α : Type*} [DecidableEq α] (a : α×Bool) (as : List (α × Bool)) (has1 : IsReduced as) (has2 : as ≠ []) (ha : a.1 ≠ (as.head has2).1) : IsReduced (a::as) := by
+  induction as using reduce.eq_1
+  -- match as with
+  -- | b::bs =>
+  --   simp at ha
+  --   rw [IsReduced]
+  --   simp [has1,ha]
+
+
+
+
+
+
+
+
+theorem IsReduced_iff_step_self {α : Type*} [DecidableEq α] (L : List (α×Bool)) : IsReduced L ↔ Red.Step L L
+
+
+
+
+/-
+
+
+
+lemma reduce_l1 {α : Type*} [DecidableEq α] (a b : α×Bool) (l : List (α × Bool)) (hl2 : reduce (b::l) = (b::l)) (hab : a.1 ≠ b.1) :
+  reduce (a::b::l) = (a::b::l) := by
+  simp [hl2,hab]
+
+lemma reduce_l2 {α : Type*} [DecidableEq α] (a : α×Bool) (l : List (α × Bool)) (hl1 : l ≠ []) (hl2 : reduce l = l) (ha : a.1 ≠ (l.head hl1).1) :
+  reduce (a::l) = a::l := by
+  match l with
+  | [] => contradiction
+  | b::bs => exact reduce_l1 a b bs hl2 ha
+
+-- theorem reduce_cons {α : Type*} [DecidableEq α] (a : α×Bool) (l : List (α × Bool)) (hl1 : l ≠ []) (ha = )
+
+-- theorem reduce_length_le {α : Type*} [DecidableEq α] (l : List (α × Bool)) : (reduce l).length ≤ l.length := by
+--   induction l with
+--   | nil => simp
+--   | cons a as ih =>
+--     match as with
+--     | [] => simp
+--     | b::bs =>
+--       rw [reduce.cons]
+--       sorry
+  -- match l with
+  -- | [] => simp
+  -- | [a] => simp
+  -- | a::b::as =>
+  --   rw [reduce.cons]
+  --   by_cases hab : a.1 = b.1 ∧ a.2 = !b.2
+  --   ·
+  --   sorry
+
+lemma reduce_l3 {α : Type*} [DecidableEq α] (a b : α×Bool) (l : List (α × Bool)) (hl : l ≠ []) (hab : a.1 ≠ b.1) (hbl : b.1 ≠ (l.head hl).1) :
+  reduce (a::b::l) = a::(reduce (b::l)) := by
+  -- induction l with
+  -- | nil => contradiction
+  -- | cons x xs ih =>
+  --   sorry
+  -- rw [reduce.cons]
+  -- have h1 :
+  sorry
+
+-- lemma reduce_cons {α : Type*} [DecidableEq α] (a : α×Bool) (L : List (α × Bool)) (hL : (reduce L) ≠ []) (ha : a.1 ≠ ((reduce L).head hL).1) : reduce (a::L) = a::(reduce L) := by
+
+theorem IsReduced_iff_reduce_eq_self {α : Type*} [DecidableEq α] (L : List (α × Bool)) : IsReduced L ↔ reduce L = L := by
+  induction L using IsReduced.induct with
+  | case1 => simp [IsReduced_nil]
+  | case2 b => simp [IsReduced_singleton]
+  | case3 a b bs ih =>
+    by_cases hbs : bs = []
+    · rw [hbs] at ih ⊢
+      simp [IsReduced]
+      tauto
+    · constructor
+      · intro h
+        rw [IsReduced] at h
+        have h1 := ih.1 h.1
+        rw [reduce.cons,h1]
+        simp
+        intro h2 h3
+        rcases h.2 with h4|h5
+        · contradiction
+        · rw [Bool.eq_not] at h3
+          contradiction
+      · intro h
+        rw [IsReduced]
+
+        have h1 : reduce (b::bs) = b::bs := by
+          apply reduce.min
+          apply Red.Step.to_red
+          -- rw [Red.cons_cons_iff b]
+        rw [reduce.cons] at h
+        sorry
+
+    -- dsimp [IsReduced] at hL
+    -- have h1 := ih hL.1
+    -- rw [reduce.cons]
+    -- have h2 : ¬(x.1 = y.1 ∧ x.2 = !y.2) := by
+    --   push_neg
+    --   rw [@Bool.ne_not]
+    --   have := hL.2
+    --   rcases this with h3|h4
+    --   · intro h; contradiction
+    --   · exact fun _ ↦ h4
+    -- -- rw [if_neg h2]
+    -- sorry
+
+theorem elt_isReduced {α : Type*} [DecidableEq α] (w : FreeGroup α) : IsReduced w.toWord := by
+  dsimp [IsReduced]
+  exact reduce_toWord w
+
+-- theorem IsReduced_mk {α : Type*} [DecidableEq α] (L : List (α × Bool)) (hL : IsReduced L) : mk L =
+
+theorem IsReduced_Reduced {α : Type*} [DecidableEq α] (L : List (α × Bool)) : IsReduced (reduce L) := by
+  dsimp [IsReduced]
+  exact reduce.idem
+
+#check List.sublist_of_cons_sublist
+
+
+theorem IsReduced_cons' {α : Type*} [DecidableEq α] (hd : α×Bool) (tl : List (α × Bool)) (h : IsReduced (hd::tl)) :
+  IsReduced tl := by
+  unfold IsReduced at h ⊢
+  induction tl with
+  | nil => simp
+  | cons a as ih =>
+    sorry
+
+
+theorem IsReduced_subword {α : Type*} [DecidableEq α] (L : List (α × Bool)) (hL : IsReduced L) {l : List (α×Bool)} (hl : l.Sublist L): IsReduced l := by
+  dsimp [IsReduced] at hL ⊢
+  induction l with
+  | nil => simp
+  | cons a as ih =>
+    have h1 := ih (List.sublist_of_cons_sublist hl)
+    rw [reduce.cons,h1]
+    match as with
+    | [] => simp
+    | b :: bs =>
+      simp
+
+theorem IsUncyclic_IsReduced {α : Type*} [DecidableEq α] (L : List (α × Bool)) : IsUncyclic L = IsReduced L := by sorry
+
+theorem IsCycPerm_refl {α : Type*} [DecidableEq α] (L : List (α × Bool)) :IsCycPerm L L := by
+  simp [IsCycPerm,word_to_CycPermList]
+  use L
+
+theorem IsCycPerm_symm_if_uncyclic {α : Type*} [DecidableEq α] (L1 L2 : List (α × Bool)) (hL1 : IsUncyclic L1) (hL2 : IsUncyclic L2) : IsCycPerm L1 L2 → IsCycPerm L2 L1 := by
+  intro h
+  simp [IsCycPerm,word_to_CycPermList] at h ⊢
+  sorry
+  -- exact fun a ↦ id (List.IsRotated.symm a)
 
 theorem isCycPerm_trans {α : Type*} [DecidableEq α] (L1 L2 L3 : List (α × Bool)) (hL1 : uncyclic L1) (hL2 : uncyclic L2) (hL3 : uncyclic L3) :
   isCycPerm L1 L2 hL1 hL2 → isCycPerm L2 L3 hL2 hL3 → isCycPerm L1 L3 hL1 hL3 := by
@@ -411,3 +591,7 @@ theorem isCycPerm_trans {α : Type*} [DecidableEq α] (L1 L2 L3 : List (α × Bo
 -- def List.isCPerm {α : Type*} (L1 : List α) (L2 : List α) : Prop := L1 ∈ L2.cyclicPermutations
 -- #check List.isro
 -- theorem List.isCPerm_refl {α : Type*} (L1 : List α) : isCPerm
+
+
+
+-/
