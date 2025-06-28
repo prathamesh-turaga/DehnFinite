cdf5b3cb9ff5c96eacb46aa900d0e058195d32fd
Omar <omar.2005.feb.12@gmail.com>
2025-06-28 11:37:48 +0530

    new

--- DehnFunction/Conj_cyc.lean
+++ DehnFunction/Conj_cyc.lean
@@ -67,6 +67,7 @@ theorem isRed_of_reduce {G: Type*} [DecidableEq G] (w : List (G × Bool)) : IsRe
 #check uncyc_then_comm_lists₂
 #check uncyclicmid
 #check app_red_still_red
+#check technique
 
 --basic theorem for freegroup
 theorem Red_sum_invRev {G: Type*} [DecidableEq G] (A : List (G × Bool)) : FreeGroup.reduce ( FreeGroup.invRev A ++ A) = [] := by
@@ -81,54 +82,126 @@ theorem Red_sum_invRev' {G: Type*} [DecidableEq G] (A : List (G × Bool)) : Free
   rw[FreeGroup.toWord_inj, ← FreeGroup.mul_mk, ← FreeGroup.inv_mk]
   exact Eq.symm (eq_mul_inv_of_mul_eq rfl)
 
+#check isredsubl
 
-theorem uncycred_to_red {G: Type*} [DecidableEq G] (A B: List (G × Bool)) (h_cycred: cycreduced B) (h_red: IsRed A): ∃ (g: List (G × Bool) ), IsRed g ∧ Uncycle (FreeGroup.reduce (A ++ B ++ FreeGroup.invRev A)) = FreeGroup.reduce (g ++ B ++ FreeGroup.invRev g) := by
+
+theorem cycred_conj_to_cyc' {G: Type*} [DecidableEq G] (B: List (G × Bool)) (h_cycred: cycreduced B) : ∀ (A : List (G × Bool)), (IsRed A) → Uncycle (FreeGroup.reduce (A ++ B ++ FreeGroup.invRev A)) ~r B := by
+  intro A
+  induction hA: A.length using Nat.strong_induction_on  generalizing A with
+  | h n ih =>
+  intro h_red
   by_cases h_empty_B: B = []
-  . use A
-    rw[h_empty_B]
+  . rw[h_empty_B]
     simp
     rw[Red_sum_invRev']
-    constructor
-    . exact h_red
-    . unfold Uncycle
-      rfl
-  . have h_or := uncyclicmid B A h_cycred
-    by_cases h_red3: IsRed (A ++ B ++ FreeGroup.invRev A)
-    . rw[equiv_of_reds] at h_red3
-      rw[h_red3, uncycle_conj]
-      use []
-      simp
-      constructor
-      . exact IsRed.nil
-      . unfold cycreduced at h_cycred
-        have h_unc: Uncycle (B) = B :=by
-          exact h_cycred.2
-        have h_red_B: FreeGroup.reduce (B) = B := by
-          rw[← equiv_of_reds]
-          exact h_cycred.1
-        rw[h_unc, h_red_B]
-    by_cases h_red2: IsRed (A++B)
+    unfold Uncycle
+    rfl
+  by_cases h_empty_A: A = []
+  . rw[h_empty_A]
+    simp
+    unfold cycreduced at h_cycred
+    have h_redB: FreeGroup.reduce (B) = B := by
+      rw[← equiv_of_reds]
+      exact h_cycred.1
+    rw[h_redB, h_cycred.2]
+
+  by_cases h_red3: IsRed (A ++ B ++ FreeGroup.invRev A)
+  . rw[equiv_of_reds] at h_red3
+    rw[h_red3, uncycle_conj]
+    unfold cycreduced at h_cycred
+    rw[h_cycred.2]
+  unfold cycreduced at h_cycred
+  have h_red_A_inv : IsRed (FreeGroup.invRev A) := by
     sorry
+  by_cases h_red_AB: IsRed (A ++ B)
+  . have h_not_red2: ¬ IsRed (B ++ FreeGroup.invRev A) := by
+      by_contra h_temp
+      have h_contra := app_red_still_red A B (FreeGroup.invRev A) h_red h_cycred.1 h_red_A_inv h_red_AB h_temp h_empty_B
+      contradiction
+    have h_app_tech := technique B (FreeGroup.invRev A) h_cycred.1 h_red_A_inv  h_not_red2
+    rcases h_app_tech with ⟨I, J, K, h_red_I, h_red_J, h_J_ne, h_red_K, h_red_IK, h_eq_IJ, h_eq_invRevJ_K⟩
+    rw[h_eq_IJ, h_eq_invRevJ_K]
+    have h_A: A = ((FreeGroup.invRev K) ++ J) := by
+      have h_inv_inv : FreeGroup.invRev (FreeGroup.invRev A) = A := FreeGroup.invRev_invRev
+      rw[← h_inv_inv, h_eq_invRevJ_K, FreeGroup.invRev_append, FreeGroup.invRev_invRev]
+
+    have h_cancel: FreeGroup.reduce (A ++ (I ++ J) ++ (FreeGroup.invRev J ++ K)) = FreeGroup.reduce (FreeGroup.reduce (A ++ I) ++ K) := by
+      calc
+        FreeGroup.reduce (A ++ (I ++ J) ++ (FreeGroup.invRev J ++ K)) = FreeGroup.reduce ((A ++ I) ++ (J ++ FreeGroup.invRev J) ++ K) := by
+            simp
+        _ = FreeGroup.reduce ( FreeGroup.reduce (A ++ I) ++ FreeGroup.reduce (J ++ FreeGroup.invRev J) ++ FreeGroup.reduce K) := by
+          rw[Red_over_three]
+        _ = FreeGroup.reduce (FreeGroup.reduce (A ++ I) ++ FreeGroup.reduce (K)) := by
+          rw[Red_sum_invRev']
+          simp
+      have h_kred: FreeGroup.reduce K = K := by
+        rw[← equiv_of_reds]
+        exact h_red_K
+      rw[h_kred]
+    rw[h_cancel]
+    have h_Kinv_red: IsRed (FreeGroup.invRev K) := by
+      sorry
+    have h_kinvinv: FreeGroup.invRev (FreeGroup.invRev K) = K := FreeGroup.invRev_invRev
+    by_cases h_empty_I: I = []
+    . rw[h_empty_I]
+      simp
+      rw[equiv_of_reds] at h_red
+      rw[h_red]
+
+      rw[h_A]
+      rw[h_empty_I] at h_eq_IJ
+      simp at h_eq_IJ
+      rw[← h_eq_IJ]
+      have h_Klen: (FreeGroup.invRev (K)).length < A.length := by
+        have h_lensum: (FreeGroup.invRev K).length + J.length = A.length := by
+          rw[h_A]
+          exact Eq.symm List.length_append
+        have h_lenJ : J.length >0 := by
+          exact List.length_pos_iff.mpr h_J_ne
+        have h_len_subt: A.length - (FreeGroup.invRev K).length > 0 := by
+          rw[← h_lensum]
+          simp
+          exact h_lenJ
+        exact Nat.lt_of_sub_pos h_len_subt
+
+      let m := (FreeGroup.invRev K).length
+      have h_m: m = (FreeGroup.invRev K).length := by
+        rfl
+      rw[hA, ← h_m] at h_Klen
+      symm at h_m
+
+      nth_rewrite 2 [← h_kinvinv]
+      exact ih m h_Klen (FreeGroup.invRev K) h_m h_Kinv_red
+
+    have h_red_AI : IsRed (A ++ I) := by
+      rw[h_eq_IJ, ← List.append_assoc] at h_red_AB
+      apply isredsubl at h_red_AB
+      exact h_red_AB.1
+    rw[equiv_of_reds] at h_red_AI
+    rw[h_red_AI]
+    rw[← equiv_of_reds] at h_red_AI
+    have h_red_aik: IsRed (A ++ I ++ K) := app_red_still_red A I K h_red h_red_I h_red_K h_red_AI h_red_IK h_empty_I
+    rw[equiv_of_reds] at h_red_aik
+    rw[h_red_aik]
+    rw[h_A]
+    have h_rw: FreeGroup.invRev K ++ J ++ I ++ K = FreeGroup.invRev K ++ (J ++ I) ++ K := by
+      simp
+    rw[h_rw]
+    nth_rewrite 2 [← h_kinvinv]
+    rw[uncycle_conj]
+    have h_uncyc_ji: cycreduced (J++I):=by
+      sorry
+    unfold cycreduced at h_uncyc_ji
+    rw[h_uncyc_ji.2]
+    exact List.isRotated_append
+
+
+
+  sorry
+
 
 theorem cycred_conj_to_cyc {G: Type*} [DecidableEq G] (A B: List (G × Bool)) (h_cycred: cycreduced B) (h_red: IsRed A): Uncycle (FreeGroup.reduce (A ++ B ++ FreeGroup.invRev A)) ~r B := by
-  have lem := uncycred_to_red A B h_cycred h_red
-  rcases lem with ⟨g, h_red_g, h_uncyc⟩
-  rw[h_uncyc]
-  let r := FreeGroup.reduce (g ++ B ++ FreeGroup.invRev g)
-  have h_r_cycred: cycreduced r := by
-    unfold cycreduced
-    constructor
-    . rw[equiv_of_reds]
-      unfold r
-      exact FreeGroup.reduce.idem
-
-    . rw[← uncycled_iff]
-      unfold r
-      rw[← h_uncyc]
-      exact uncycled_of_Uncycle (FreeGroup.reduce (A ++ B ++ FreeGroup.invRev A))
-  exact
-    prathamesh_lemma (FreeGroup.reduce (g ++ B ++ FreeGroup.invRev g)) B g h_r_cycred h_cycred
-      h_red_g rfl
+  exact cycred_conj_to_cyc' B h_cycred A h_red
 
 
 
